af196451b93f0ccbff415c0e58094c282973cfa7
kezaar <125607255+kezaar@users.noreply.github.com>
2026-02-07 16:23:11 -0600

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -12,41 +12,41 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
             })
         });
-var format_POIs_1 = new ol.format.GeoJSON();
-var features_POIs_1 = format_POIs_1.readFeatures(json_POIs_1, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_POIs_1 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_POIs_1.addFeatures(features_POIs_1);
-var lyr_POIs_1 = new ol.layer.Vector({
-                declutter: false,
-                source:jsonSource_POIs_1, 
-                style: style_POIs_1,
-                popuplayertitle: 'POIs',
-                interactive: true,
-                title: '<img src="styles/legend/POIs_1.png" /> POIs'
-            });
-var lyr_PedwayBaseMap_2 = new ol.layer.Image({
+var lyr_PedwayBaseMap_1 = new ol.layer.Image({
         opacity: 1,
         
     title: 'Pedway Base Map<br />' ,
         
         
         source: new ol.source.ImageStatic({
-            url: "./layers/PedwayBaseMap_2.png",
+            url: "./layers/PedwayBaseMap_1.png",
             attributions: ' ',
             projection: 'EPSG:3857',
             alwaysInRange: true,
             imageExtent: [-9755502.598994, 5142049.313868, -9753286.633133, 5144574.348651]
         })
     });
+var format_POIs_2 = new ol.format.GeoJSON();
+var features_POIs_2 = format_POIs_2.readFeatures(json_POIs_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_POIs_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_POIs_2.addFeatures(features_POIs_2);
+var lyr_POIs_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_POIs_2, 
+                style: style_POIs_2,
+                popuplayertitle: 'POIs',
+                interactive: true,
+                title: '<img src="styles/legend/POIs_2.png" /> POIs'
+            });
 
-lyr_GoogleSatellite_0.setVisible(true);lyr_POIs_1.setVisible(true);lyr_PedwayBaseMap_2.setVisible(true);
-var layersList = [lyr_GoogleSatellite_0,lyr_POIs_1,lyr_PedwayBaseMap_2];
-lyr_POIs_1.set('fieldAliases', {'id': 'id', 'POIs': 'POIs', 'Additional': 'Additional', });
-lyr_POIs_1.set('fieldImages', {'id': 'TextEdit', 'POIs': 'TextEdit', 'Additional': '', });
-lyr_POIs_1.set('fieldLabels', {'id': 'no label', 'POIs': 'inline label - always visible', 'Additional': 'header label - visible with data', });
-lyr_POIs_1.on('precompose', function(evt) {
+lyr_GoogleSatellite_0.setVisible(true);lyr_PedwayBaseMap_1.setVisible(true);lyr_POIs_2.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_PedwayBaseMap_1,lyr_POIs_2];
+lyr_POIs_2.set('fieldAliases', {'id': 'id', 'POIs': 'POIs', 'Additional': 'Additional', });
+lyr_POIs_2.set('fieldImages', {'id': 'TextEdit', 'POIs': 'TextEdit', 'Additional': '', });
+lyr_POIs_2.set('fieldLabels', {'id': 'no label', 'POIs': 'inline label - always visible', 'Additional': 'header label - visible with data', });
+lyr_POIs_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
